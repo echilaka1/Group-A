0769c1e41d081e437550df74bd3475f6ef9cbf74
hossamhsn77 <hossam.h@aicomai.ai>
2024-12-10 13:52:29 -0600

    push 2 new users

--- src/LibrarySystem/Model/Data.java
+++ src/LibrarySystem/Model/Data.java
@@ -10,25 +10,27 @@ public class Data {
     public static final String MESSIAH_OF_DUNE = "Messiah Of Dune";
     public static final String GONE_WITH_THE_WIND = "Gone With The Wind";
     public static final String GARDEN_OF_RAMA = "Garden of Rama";
-    
-    @SuppressWarnings("serial")
-	public static List<String> bookTitles = new ArrayList<String>(){
 
-		{
+    @SuppressWarnings("serial")
+    public static List<String> bookTitles = new ArrayList<String>() {
+
+        {
             this.add(Data.MESSIAH_OF_DUNE);
             this.add(Data.GONE_WITH_THE_WIND);
             this.add(Data.GARDEN_OF_RAMA);
         }
     };
     public static Auth currentAuth = null;
-    
-    @SuppressWarnings("serial")
-	public static List<User> logins = new ArrayList<User>(){
 
-		{
+    @SuppressWarnings("serial")
+    public static List<User> logins = new ArrayList<User>() {
+
+        {
             this.add(new User("Joe", "111", Auth.LIBRARIAN));
             this.add(new User("Ann", "101", Auth.ADMIN));
             this.add(new User("Dave", "102", Auth.BOTH));
+            this.add(new User("emannuel", "123", Auth.BOTH));
+            this.add(new User("hossam", "123", Auth.BOTH));
         }
     };
 
@@ -39,4 +41,3 @@ public class Data {
         bookTitles.add(title);
     }
 }
-
