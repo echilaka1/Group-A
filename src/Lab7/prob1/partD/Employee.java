e48e8849f0b595ec9edbb108885739c168f43c1e
Emmanuel Chilaka <emmanuelchilaka779@gmail.com>
2024-12-11 23:05:29 -0600

    finished lab 7

--- src/Lab7/prob1/partD/Employee.java
+++ src/Lab7/prob1/partD/Employee.java
@@ -30,24 +30,19 @@ public class Employee {
 	public String toString() {
 		return "(" + name + ", " + salary + ")";
 	}
-	
+
 	@Override
 	public boolean equals(Object ob) {
 		if(ob == null) return false;
 		if(!(ob instanceof Employee)) return false;
 		Employee emp = (Employee)ob;
-		return emp.name.equals(name) && emp.salary == salary && emp.visited == visited;
+		return emp.name.equals(name) && emp.salary == salary;
 	}
-	
 	@Override
 	public int hashCode() {
 		int result = 17;
-		long longval = Double.doubleToLongBits(salary);
-		int salaryHash = (int) (longval ^ (longval >>> 32));
-		result += 31 * result + name.hashCode();
+		long longval = Double.doubleToLongBits(salary); int salaryHash = (int) (longval ^ (longval >>> 32)); result += 31 * result + name.hashCode();
 		result += 31 * result + salaryHash;
-		result += 31 * result + (visited ? 1 : 0);
-		//result += 31 * result + (visited ? 71 : 43);
 		return result;
 	}
 }
